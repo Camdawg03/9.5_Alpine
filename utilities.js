e21e63d67ca178e1cfd33a0175e0b075573200a8
Camdawg03 <133887963+Camdawg03@users.noreply.github.com>
2025-03-20 15:32:07 +0000

    changes

--- utilities.js
+++ utilities.js
@@ -1,7 +1,7 @@
 function rando() {
     const names = ['Ayla', 'Ryder', 'Grey', 'Crosby', 'Peyton', 'Bailee', 'Caitlyn', 'Bradley', 'Yvonne', 'Cameron'];
     return {
-        get name() {
+        getname() {
             return names[Math.floor(Math.random()* names.length)];
         },
     }
